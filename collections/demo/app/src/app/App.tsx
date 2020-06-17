b78030a879af4606174f7b88c1affcc9badd0e32
- <->
2020-06-17 15:07:58 +0200

    Update demo app with some css

---
+++ collections/demo/app/src/app/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Channels from "./Channels";
+import Messages from "./Messages";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Channels />
+      <Messages />
+    </div>
+  );
+}
+
+export default App;
