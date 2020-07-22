88363da06f7f1ae1c49a71fe160672c2569d33d1
Pierre Tessier <pierre.tessier@polytechnique.edu>
2020-07-22 10:18:57 +0200

    bug correction : rabbitmq error 406 with acks

--- collections/server/src/MessageBroker/MessageBrokerInterface.ts
+++ collections/server/src/MessageBroker/MessageBrokerInterface.ts
@@ -1,4 +1,10 @@
 import * as amqp from "amqplib";
+import { connect } from "http2";
+
+type ExternalResolver = {
+    res:() => void,
+    rej:() => void
+}
 
 export abstract class TaskBroker
 {
@@ -21,22 +27,45 @@ export class RabbitMQBroker extends TaskBroker
 {
     amqpURL:string;
     channel?:amqp.Channel;
+    connectionHandling:boolean;
     callback:(path:string)=>Promise<void>;
+
+    delayerResolver?:ExternalResolver;
+    delayerPromise?:Promise<unknown>;
 
     constructor(amqpURL:string, queueName:string, callback:(path:string)=>Promise<void>, queueOptions:Object, messageOptions:Object)
     {
         super(queueName, queueOptions, messageOptions);
         this.amqpURL = amqpURL;
         this.callback = callback;
+        this.connectionHandling = false;
     }
 
     public async pushTask(task:string):Promise<void>
     {
         if(this.channel === undefined)
         {
-            let conn = await amqp.connect(this.amqpURL);
-            this.channel = await conn.createChannel();
+            if(!this.connectionHandling)
+            {
+                this.connectionHandling = true;
+                let delayer:ExternalResolver = {res: ()=>{}, rej: ()=>{}};
+                this.delayerPromise = new Promise(function (resolve, reject):void {
+                    delayer.res = resolve;
+                    delayer.rej = reject;
+                });
+                this.delayerResolver = delayer;
+    
+                let conn = await amqp.connect(this.amqpURL);
+                this.channel = await conn.createChannel();
+    
+                this.delayerResolver.res();
+            }
+            if(this.connectionHandling)
+            {
+                await this.delayerPromise;
+            }
         }
+        if(this.channel === undefined) throw new Error("Missing channel");
         await this.channel.assertQueue(this.queueName, this.queueOptions);
         this.channel.sendToQueue(this.queueName, Buffer.from(task, "utf-8"), this.messageOptions);
     }
